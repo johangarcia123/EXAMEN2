c2b4b9ab70dcb9b38cd27189edf78f0f47329f18
johangarcia123 <johanngarciavizuete@gmail.com>
2023-02-06 22:36:31 -0500

    SUBIR
    EXAMEN

---
+++ js/sweeterAlert.js
@@ -0,0 +1,8 @@
+Swal.fire({
+    title: 'Gracias!',
+    text: 'Llena el formulario para contactarnos.',
+    imageUrl: 'https://previews.123rf.com/images/dmitryag/dmitryag2104/dmitryag210408530/167649228-mujer-en-camiseta-blanca-hamburguesa-comida-r%C3%A1pida-dieta-comida-delicadeza.jpg',
+    imageWidth: 400,
+    imageHeight: 200,
+    imageAlt: 'Custom image',
+  })
---
+++ js/swetA.js
@@ -0,0 +1,8 @@
+Swal.fire({
+    title: 'Gracias!',
+    text: 'Esto es un poco de nosotros un gusto.',
+    imageUrl: 'https://previews.123rf.com/images/dmitryag/dmitryag2104/dmitryag210408530/167649228-mujer-en-camiseta-blanca-hamburguesa-comida-r%C3%A1pida-dieta-comida-delicadeza.jpg',
+    imageWidth: 400,
+    imageHeight: 200,
+    imageAlt: 'Custom image',
+  })
